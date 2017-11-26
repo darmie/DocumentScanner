9e28107eb5f13ac23bd383fa15ffd5452c6a9642
Damilare Darmie Akinlaja <dreakinlaja@gmail.com>
2017-11-26 14:15:06 +0100

    added socket.io

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +24,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+server.listen(80);
+io.on('connection', function (socket) {
+  socket.emit('connected', { session_key: '' });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
